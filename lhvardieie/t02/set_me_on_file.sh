a0477d2b980a6291142a94b7ba1feb2c5a623877
lhvardieie <76736798+lhvardieie@users.noreply.github.com>
2021-01-03 17:42:41 +0200

    Add files via upload

---
+++ lhvardieie/t02/set_me_on_file.sh
@@ -0,0 +1,5 @@
+touch fire
+chmod u=r fire
+chmod g=- fire
+chmod o=- fire
+touch -mt 190008241314 /Users/lhvardieie/desktop/lhvardieie/t02 fire
---
+++ lhvardieie/t08/alias.sh
@@ -0,0 +1,6 @@
+echo "alias ga='git add'
+alias gcmsg='git commit -m'
+alias gp='git push'" > source_me
+
+
+
